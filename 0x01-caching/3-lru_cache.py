93f093a597977c922eccbcd6ed64b696fb1e3a54
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-20 01:45:24 +0300

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -12,7 +12,7 @@ class LRUCache(base_caching.BaseCaching):
     """
     def __init__(self):
         super().__init__()
-        self.usage = {}
+        self.recent = []
 
     def put(self, key, item):
         """
@@ -20,17 +20,13 @@ class LRUCache(base_caching.BaseCaching):
         """
         if key is None or item is None:
             return None
-        if len(self.cache_data) == self.MAX_ITEMS:
-            disKey = list(self.cache_data.keys())[0]
-            usageRate = self.usage[disKey]
-            for key in self.cache_data.keys():
-                if self.usage[key] < usageRate:
-                    disKey = key
-                    usageRate = self.usage[key]
+        if key in self.cache_data.keys():
+            self.recent.pop(self.recent.index(key))
+        elif len(self.cache_data) == self.MAX_ITEMS:
+            disKey = self.recent.pop(0)
             self.cache_data.pop(disKey)
-            print(f"DISCARD: {disKey}")
-        self.usage.update({key: 0})
         self.cache_data.update({key: item})
+        self.recent.append(key)
 
     def get(self, key):
         """
@@ -38,5 +34,6 @@ class LRUCache(base_caching.BaseCaching):
         """
         if key is None or self.cache_data.get(key) is None:
             return None
-        self.usage[key] += 1
+        self.recent.pop(self.recent.index(key))
+        self.recent.append(key)
         return self.cache_data.get(key)
