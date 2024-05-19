183bebfc5ab592b26939ed904018cfd23121332a
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-20 01:54:19 +0300

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module for task 'MRU Caching'
+"""
+
+import base_caching
+
+
+class MRUCache(base_caching.BaseCaching):
+    """
+    This class is used to create an instance of a MRU cache.
+    """
+    def __init__(self):
+        super().__init__()
+        self.recent = []
+
+    def put(self, key, item):
+        """
+        This method adds an item to the cache.
+        """
+        if key is None or item is None:
+            return None
+        if key in self.cache_data.keys():
+            self.recent.pop(self.recent.index(key))
+        elif len(self.cache_data) == self.MAX_ITEMS:
+            disKey = self.recent.pop(-1)
+            self.cache_data.pop(disKey)
+            print(f"DISCARD: {disKey}")
+        self.cache_data.update({key: item})
+        self.recent.append(key)
+
+    def get(self, key):
+        """
+        This method gets an item from the cache.
+        """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        self.recent.pop(self.recent.index(key))
+        self.recent.append(key)
+        return self.cache_data.get(key)
