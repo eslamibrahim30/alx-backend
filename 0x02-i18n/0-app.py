cab30d5a449da0e7f6df44eef70bb6f49cede032
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-05-22 21:15:21 +0300

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module for task 'Basic Flask app'
+"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """
+    This function returns the root html file
+    """
     return render_template('./templates/0-index.html')
